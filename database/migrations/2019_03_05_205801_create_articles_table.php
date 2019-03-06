9023e14c2817e89813b5945e0e6bf8f2dd0c0d09
Tillmann <tillmann1970@gmail.com>
2019-03-06 14:43:23 +0100

    Döpte om en migration som hette fel

--- database/migrations/2019_03_05_205801_create_articles_table.php
+++ database/migrations/2019_03_05_205801_create_articles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateArticleCategoryTable extends Migration
+class CreateArticlesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -34,6 +34,6 @@ class CreateArticleCategoryTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('article_category');
+        Schema::dropIfExists('articles');
     }
 }
