69e3992468cbe075472c3daf4b3a4a95a3715b36
Philip Angelin <philip_angelin@hotmail.com>
2019-03-07 14:18:15 +0100

    Test

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Route::resource('/articles', 'ArticleController');
 Route::resource('/locations', 'LocationController');
 Route::resource('/articles', 'ArticleController');
