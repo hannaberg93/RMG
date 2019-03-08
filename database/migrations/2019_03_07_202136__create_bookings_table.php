27a02e6903c16154b53d68415577b91ed429e6b4
Philip Angelin <philip_angelin@hotmail.com>
2019-03-08 11:25:53 +0100

    Create Bookings table fix

--- database/migrations/2019_03_07_202136__create_bookings_table.php
+++ database/migrations/2019_03_07_202136__create_bookings_table.php
@@ -6,23 +6,34 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateBookingsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        //
-    }
+   /**
+    * Run the migrations.
+    *
+    * @return void
+    */
+   public function up()
+   {
+       Schema::create('bookings', function (Blueprint $table) {
+           $table->bigIncrements('id');
+           $table->string('name');
+           $table->string('address');
+           $table->string('city');
+           $table->string('phone');
+           $table->string('email');
+           $table->text('message');
+           $table->dateTime('date_start');
+           $table->dateTime('date_end');
+           $table->timestamps();
+       });
+   }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        //
-    }
+   /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+   public function down()
+   {
+       Schema::dropIfExists('bookings');
+   }
 }
