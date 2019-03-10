9f124f8acf48020576c563229b9f02bacd93848b
Tillmann <tillmann1970@gmail.com>
2019-03-10 21:56:11 +0100

    små ändringar på 'mina sidor'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,18 +8,19 @@
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>  --}}
 
-                                <div>
+<div>
 
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+    </form>
+</div>
                             {{--  </li>  --}}
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card mt-3 col-12">
+            <div class="card mt-3 col-12 p-3 bg-light text-dark">
+
                 <h4>Välkommen {{ Auth::user()->name }}<br></h4><p> Du är inloggad!</p><hr>
                 <h5>Mina sidor</h5>
 
@@ -38,15 +39,16 @@
 
 
 
-                <p>Uthyrda artiklar:</p>
+
 
                 <p>Bokningsförfrågningar:</p>
 
-                <p>Artikler som jag hyr:</p>
+                <p>Artikler som jag hyr:</p><hr>
 
-                <button type="button" class="btn btn-success col-3"><a href="/articles/create">Hyra ut en artikel</a></button>
 
                 <div class="card-body">
+                <button class="btn btn-secondary col-3.5"><a href="/articles/index">Boka en artikel</a></button>
+                <button type class="btn btn-secondary col-3 mt-10"><a href="/articles/create">Hyra ut en artikel</a></button>
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -55,20 +57,25 @@
 
 
                 </div>
+
+                <a  class="btn btn-danger col-2" href="{{ route('logout') }}"
+                                                       onclick="event.preventDefault();
+                                                                     document.getElementById('logout-form').submit();">
+                                                        {{ __('Logout') }}
+                                                    </a>
+
             </div>
         </div>
     </div>
 </div>
-<a  class="btn btn-danger" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+
 
 <style>
 
     a{
         color: white;
-    }
+    };
+
+
 </style>
 @endsection
